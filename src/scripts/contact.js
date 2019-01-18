a03e4db2acf8d9aaff9da89c8d9d97faaa666ef2
JMONDESIR <Joey.M.Dev@gmail.com>
2019-01-18 13:42:43 -0600

    second commit

---
+++ src/scripts/contact.js
@@ -0,0 +1,12 @@
+//  CONTAINS TEMPLATE THAT CREATES AND RETURNS CONTACT HTML COMPONENTS AND DISPLAYS ONE PERSON'S INFO (DOM BUILDER)
+
+// contactArray.forEach(function(eachContact) {
+//   console.log(eachContact);
+// });
+const contact = {
+  makeDOM(contactObject) {
+    console.table(contactObject);
+  }
+};
+
+export default contact;
---
+++ src/scripts/main.js
@@ -0,0 +1,4 @@
+// This is where Browserfy starts and is the central component of the application
+import contactList from "./contantList";
+
+contactList.getAndAppendContacts();
