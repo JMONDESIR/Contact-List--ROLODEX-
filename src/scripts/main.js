3c131ce1c096e52548e630d8e3df5247f21b0f25
JMONDESIR <Joey.M.Dev@gmail.com>
2019-02-04 13:19:07 -0600

    first commit

--- src/scripts/main.js
+++
@@ -1,7 +0,0 @@
-// This is where Browserfy starts and is the central component of the application
-import contactList from "./contantList";
-import contactForm from "./contactForm";
-
-contactList.getAndAppendContacts();
-contactForm.formBuilder();
-//--------------------------------------------------------------------------------------
